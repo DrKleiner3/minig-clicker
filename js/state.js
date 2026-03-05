f20e6f5179ff30a03adc0295af661fb614139029
DrKleiner3 <germansuperproductiontm@gmail.com>
2026-03-05 10:53:29 +0100

    Increase MAP_HEIGHT from 10 to 13

--- js/state.js
+++ js/state.js
@@ -9,7 +9,7 @@
 export const TILE_SIZE = 64;
 
 export let MAP_WIDTH = 12;
-export let MAP_HEIGHT = 10;
+export let MAP_HEIGHT = 13;
 
 export const AOE_INTERVAL_MS = 200;
 
@@ -311,4 +311,5 @@ export function getLevelBoostSavePatch() {
     activeBoosts,
   };
 }
+
 
